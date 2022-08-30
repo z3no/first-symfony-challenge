1385bc54f1e04784892ec10578afd74edd359b2e
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-30 15:38:57 +0200

    created a user model with a name property which is set to unknown, added getter and setter

---
+++ src/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+class User
+{
+    private $name = 'Unknown';
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
