ef8a1ebbc3a315f632d889b17d0622b4e85f6fd7
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-30 16:52:13 +0200

    rendered form getting hello and the name variable that is set to unknown, also underneath we get a form with an input field for name, with placeholder text from the name property and a button change user name. Next up is looking into on how to process the form

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,6 +28,25 @@ class LearningController extends AbstractController
         $user = new User();
         $name = $user->getName();
 
+        $form = $this->createFormBuilder($user)
+//            ->setAction($this->generateUrl('/change-my-name'))
+//            ->setMethod('POST')
+            ->add('name', TextType::class)
+            ->add('Submit', SubmitType::class, ['label' => 'Change user name'])
+            ->getForm();
+
+
+        return $this->renderForm('base.html.twig', [
+            'name' => $name,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/change-my-name')]
+    public function changeMyName() : Response
+    {
+        $user = new User();
+        $name = $user->setName();
         return $this->render('base.html.twig', [
             'name' => $name,
         ]);
